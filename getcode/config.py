cd9fd7e058d7c194aca52c77f85d5b2ec70120a0
Code Yard <arduinoleo88@gmail.com>
2020-06-03 14:49:16 +0530

    Fixed UI Issues

--- getcode/config.py
+++ getcode/config.py
@@ -15,7 +15,7 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 # Flask-Mail setup to use GMail
 MAIL_SERVER=os.environ.get('MAIL_SERVER')
 MAIL_PORT=os.environ.get('MAIL_PORT')
-MAIL_USE_SSL=True
+MAIL_USE_TLS=True
 MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
 MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
 
